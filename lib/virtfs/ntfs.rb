bf53abefbe6749bbac66a14af84b028a7435d1ed
Jason Frey <fryguy9@gmail.com>
2016-11-28 17:10:48 -0500

    Add base files for new gem.

---
+++ lib/virtfs/ntfs.rb
@@ -0,0 +1,7 @@
+require "virtfs/ntfs/version"
+
+module Virtfs
+  module Ntfs
+    # Your code goes here...
+  end
+end
---
+++ lib/virtfs/ntfs/version.rb
@@ -0,0 +1,5 @@
+module Virtfs
+  module Ntfs
+    VERSION = "0.1.0"
+  end
+end
